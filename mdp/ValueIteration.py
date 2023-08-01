ca2aec05bc8b743d5dc283705d012ce65fb7eec7
omroot <oualid.missaoui@gmail.com>
2023-08-01 16:08:43 -0400

    refactor value iteration into a calss

--- mdp/ValueIteration.py
+++ mdp/ValueIteration.py
@@ -1,91 +1,89 @@
 import numpy as np
-from mdp.utils.visualization import show_value_function_progress
-def value_iteration(env_info,
-                    gamma: float,
-                    num_iters: int):
-    env_desc = env_info['desc']  # 2D array shows what each item means
-    prob_idx = env_info['trans_prob_idx']
-    nextstate_idx = env_info['nextstate_idx']
-    reward_idx = env_info['reward_idx']
-    num_states = env_info['num_states']
-    num_actions = env_info['num_actions']
-    mdp = env_info['mdp']
+import attr
 
-    V  = np.zeros((num_iters + 1, num_states))
-    Q  = np.zeros((num_iters + 1, num_states, num_actions))
-    pi = np.zeros((num_iters + 1, num_states))
+@attr.s
+class ValueIteration:
+    env = attr.ib()
+    gamma = attr.ib(default=1.0)
+    threshold = attr.ib(default=1e-6)
+    max_iterations = attr.ib(default=10000)
+    inplace = attr.ib(default=False)  # New attribute for algorithm version
 
-    for k in range(1, num_iters + 1):
-        for s in range(num_states):
-            for a in range(num_actions):
-                # Calculate \sum_{s'} p(s'\mid s,a) [r + \gamma v_k(s')]
-                for pxrds in mdp[(s,a)]:
-                    # mdp(s,a): [(p1,next1,r1,d1),(p2,next2,r2,d2),..]
-                    pr = pxrds[prob_idx]  # p(s'\mid s,a)
-                    nextstate = pxrds[nextstate_idx]  # Next state
-                    reward = pxrds[reward_idx]  # Reward
-                    Q[k,s,a] += pr * (reward + gamma * V[k - 1, nextstate])
-            # Record max value and max action
-            V[k,s] = np.max(Q[k,s,:])
-            pi[k,s] = np.argmax(Q[k,s,:])
-    show_value_function_progress(env_desc, V[:-1], pi[:-1])
-    return V, pi
+    optimal_state_values = attr.ib(default=None)
+    optimal_policy = attr.ib(default=None)
+
+    def fit(self) -> None:
+        if self.inplace:
+            self.optimal_state_values = self._inplace_value_iteration()
+        else:
+            self.optimal_state_values = self._state_sweep_value_iteration()
+
+        self.optimal_policy = self.extract_policy(self.optimal_state_values)
 
 
+    def extract_policy(self, value_table: np.ndarray, gamma: float = 1.0) -> np.ndarray:
+        policy = np.zeros(self.env.observation_space.n, dtype=int)
+
+        for state in range(self.env.observation_space.n):
+            Q_table = np.zeros(self.env.action_space.n)
+
+            for action in range(self.env.action_space.n):
+                for next_sr in self.env.P[state][action]:
+                    trans_prob, next_state, reward_prob, _ = next_sr
+                    Q_table[action] += (trans_prob * (reward_prob + gamma * value_table[next_state]))
+
+            policy[state] = np.argmax(Q_table)
+
+        return policy
+
+    def _inplace_value_iteration(self):
+        value_table = np.zeros(self.env.observation_space.n)
+
+        for i in range(self.max_iterations):
+            max_delta = 0.0
+
+            for state in range(self.env.observation_space.n):
+                old_state_value = value_table[state]
+                value_table[state] = self._calculate_state_value(state, value_table)
+                max_delta = max(max_delta, np.abs(value_table[state] - old_state_value))
+
+            if max_delta < self.threshold:
+                print(f'Value-iteration (in-place) converged at iteration #{i + 1}.')
+                break
+
+        return value_table
+
+    def _state_sweep_value_iteration(self):
+        value_table = np.zeros(self.env.observation_space.n)
+        updated_value_table = np.zeros(self.env.observation_space.n)
+
+        for i in range(self.max_iterations):
+            max_delta = 0.0
+
+            for state in range(self.env.observation_space.n):
+                updated_value_table[state] = self._calculate_state_value(state, value_table)
+                max_delta = max(max_delta, np.abs(updated_value_table[state] - value_table[state]))
+
+            value_table = np.copy(updated_value_table)
+
+            if max_delta < self.threshold:
+                print(f'Value-iteration (state sweep) converged at iteration #{i + 1}.')
+                break
+
+        return value_table
+
+    def _calculate_state_value(self, state, value_table):
+        Q_value = []
+
+        for action in range(self.env.action_space.n):
+            next_states_rewards = []
+            for next_sr in self.env.P[state][action]:
+                trans_prob, next_state, reward_prob, _ = next_sr
+                next_states_rewards.append(trans_prob * (reward_prob + self.gamma * value_table[next_state]))
+
+            Q_value.append(np.sum(next_states_rewards))
+
+        return max(Q_value)
 
 
-def synchronous_value_iteration(env_info,
-                    gamma: float,
-                    num_iters: int):
-    env_desc = env_info['desc']  # 2D array shows what each item means
-    prob_idx = env_info['trans_prob_idx']
-    nextstate_idx = env_info['nextstate_idx']
-    reward_idx = env_info['reward_idx']
-    num_states = env_info['num_states']
-    num_actions = env_info['num_actions']
-    mdp = env_info['mdp']
 
-    V  = np.zeros((num_iters + 1, num_states))
-    Q  = np.zeros((num_iters + 1, num_states, num_actions))
-    pi = np.zeros((num_iters + 1, num_states))
-
-    for k in range(1, num_iters + 1):
-        old_Q = Q.copy()
-        old_V = V.copy()
-        for s in range(num_states):
-            for a in range(num_actions):
-                # Calculate \sum_{s'} p(s'\mid s,a) [r + \gamma v_k(s')]
-                for pxrds in mdp[(s,a)]:
-                    # mdp(s,a): [(p1,next1,r1,d1),(p2,next2,r2,d2),..]
-                    pr = pxrds[prob_idx]  # p(s'\mid s,a)
-                    nextstate = pxrds[nextstate_idx]  # Next state
-                    reward = pxrds[reward_idx]  # Reward
-                    Q[k,s,a] += pr * (reward + gamma * old_V[k - 1, nextstate])
-            # Record max value and max action
-            V[k,s] = np.max(Q[k,s,:])
-            pi[k,s] = np.argmax(Q[k,s,:])
-    show_value_function_progress(env_desc, V[:-1], pi[:-1])
-    return V, pi
-
-
-
-#
-#
-# def synchronous_value_iteration(states, actions, transition_probabilities, rewards, gamma, epsilon):
-#     v = np.zeros(len(states))
-#     while True:
-#         delta = 0
-#         old_v = v.copy()
-#         for s in states:
-#             v_new = 0
-#             for a in actions:
-#                 sum_prob = 0
-#                 for s_prime, p in transition_probabilities[s][a].items():
-#                     sum_prob += p * rewards[s_prime] + gamma * old_v[s_prime]
-#                 v_new = max(v_new, sum_prob)
-#             delta = max(delta, abs(v_new - old_v[s]))
-#             v[s] = v_new
-#         if delta < epsilon:
-#             break
-#     return v
-#
